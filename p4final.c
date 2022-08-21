ba4929357477df3e888af24b4282836ee2fed19b
SPANDANA AP <110383668+spandana2004@users.noreply.github.com>
2022-08-21 08:17:13 +0000

    compare 2 strings

---
+++ p4final.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void input_two_string(char a[],char b[])
+{
+      printf("enter string 1:");
+      scanf("%s",a);
+      printf("enter string 2:");
+      scanf("%s",b);
+}
+int srtcmp(char a[],char b[])
+{
+  int result=0;
+  for (int i=0;((a[i]>'\0')&&(b[i]>'\0'));i++)
+    {
+      if(a[i]==b[i])
+      {
+        result=0;
+      }
+      else if (a[i]>b[i])
+      {
+        result=1;
+        return result;
+      }
+      else
+      {
+        result=2;
+        return result;
+      }
+    }
+}
+void output(char a[],char  b[],int result)
+{
+  if(result==0)
+  {
+    printf("the strings are same");
+  }
+  else if(result==1)
+  {
+    printf("%s is greater than %s",a,b);
+  }
+  else 
+  {
+    printf("%s is greater than %s",b,a);
+  }
+}
+int main()
+{
+int result;
+  char a[20],b[20];
+  input_two_string(a,b);
+  result=srtcmp(a,b);
+  output(a,b,result);
+}
